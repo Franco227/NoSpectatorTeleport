6c8efeb88ade246ee60334f53edcb157853cafaf
Franco227 <227franco@gmail.com>
2024-11-01 22:05:26 +0100

    feat: initial release

---
+++ src/main/java/com/example/franco227/no_spectator_teleport/NoSpectatorTeleport.java
@@ -0,0 +1,16 @@
+package com.example.franco227.no_spectator_teleport;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoSpectatorTeleport implements ModInitializer {
+	public static final String MOD_ID = "no-spectator-teleport";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Disabled spectator teleporting.");
+	}
+}
---
+++ src/main/java/com/example/franco227/no_spectator_teleport/mixin/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,20 @@
+package com.example.franco227.no_spectator_teleport.mixin;
+
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.SpectatorTeleportC2SPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public abstract class ServerPlayNetworkHandlerMixin {
+
+    @Inject(at = @At("RETURN"), method = "accepts", cancellable = true)
+    private void accepts(Packet<?> packet, CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValue() && (packet instanceof SpectatorTeleportC2SPacket)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
