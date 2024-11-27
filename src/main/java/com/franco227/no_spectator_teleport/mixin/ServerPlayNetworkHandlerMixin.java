5165a1ca943deb829b918c7c5a849943cd69a7b9
Franco227 <227franco@gmail.com>
2024-11-27 21:31:41 +0100

    build: add support for Minecraft 1.17 to latest

--- src/main/java/com/franco227/no_spectator_teleport/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/franco227/no_spectator_teleport/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,20 +1,17 @@
 package com.franco227.no_spectator_teleport.mixin;
 
-import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.SpectatorTeleportC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin {
 
-    @Inject(at = @At("RETURN"), method = "accepts", cancellable = true)
-    private void accepts(Packet<?> packet, CallbackInfoReturnable<Boolean> cir) {
-        if (cir.getReturnValue() && (packet instanceof SpectatorTeleportC2SPacket)) {
-            cir.setReturnValue(false);
-        }
+    @Inject(at = @At("HEAD"), method = "onSpectatorTeleport", cancellable = true)
+    private void onSpectatorTeleport(SpectatorTeleportC2SPacket packet, CallbackInfo ci) {
+        ci.cancel();
     }
 }
